237d9fddd5822e796bb5e69944b65b46bd6cf2fb
Daniel <danielb1194@gmail.com>
2026-07-10 19:21:55 -0500

    Add debug logging settings to ModSettings

--- ModSettings.cs
+++ ModSettings.cs
@@ -2,6 +2,10 @@ using Menace.SDK;
 
 public static class _MOD_NAME_ModSettings
 {
+    public static readonly bool DEFAULT_IS_DEBUG_LOGGING = false;
+
+    public static readonly string DEBUG_LOGGING_KEY = "_MOD_NAME_DebugLogging";
+
     /// <summary>
     /// Configures the mod settings for the _MOD_NAME_ Mod.
     /// </summary>
@@ -15,7 +19,12 @@ public static class _MOD_NAME_ModSettings
             settings =>
             {
                 // Settings for accuracy when flanking
-                settings.AddHeader("_MOD_NAME_ Settings");
+                settings.AddHeader("_MOD_NAME_ Logging Settings");
+                settings.AddToggle(
+                    DEBUG_LOGGING_KEY,
+                    "Enable or disable debug logging for the _MOD_NAME_ mod.",
+                    DEFAULT_IS_DEBUG_LOGGING
+                );
             }
         );
     }
